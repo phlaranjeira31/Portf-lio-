18b1eb2476add97cf75557a5e8839f1501bcf4f5
phlaranjeira <pedrolaranjeira09@gmail.com>
2025-11-25 10:57:21 -0300

    ajusta texto da seção Sobre mim

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
         <p className="about-text">
     Sou um desenvolvedor em formação, focado em desenvolvimento full stack, integração com APIs, modelagem de banco de dados e aplicações que utilizam inteligência artificial. Tenho experiência prática com tecnologias como JavaScript, React, Next.js, Node.js e SQL, sempre buscando escrever código limpo, escalável e orientado a boas práticas. Meu objetivo é evoluir profissionalmente como engenheiro de software, trabalhando em soluções eficientes, de alto impacto e que utilizem tecnologia de ponta.
           <br /><br />
-          Tenho experiência em desenvolvimento de sistemas em projetos pessoais e também profissionais. Busco sempre dar o meu melhor e estou sempre disposto a conhecer novas tecnologias. 
+           Atualmente, busco oportunidades como desenvolvedor front-end ou fullstack, onde eu possa contribuir com soluções eficientes, escaláveis e orientadas a boas práticas. Tenho grande interesse em atuar em equipes que valorizem aprendizado contínuo, autonomia e impacto real no produto. 
         </p>
       </div>
     </section>
